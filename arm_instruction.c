535f188858939275c26e65231caddad1058b5ab0
jeremiefiniell3 <jeremie.finiel@laposte.net>
2015-01-06 16:05:44 +0100

    ajout de log

--- arm_instruction.c
+++ arm_instruction.c
@@ -28,6 +28,8 @@ Contact: Guillaume.Huard@imag.fr
 #include "arm_branch_other.h"
 #include "arm_constants.h"
 #include "util.h"
+#include <debug.h>
+#include "arm_core.h"
 
 // Fields parsing
 
@@ -92,20 +94,30 @@ static int arm_execute_instruction(arm_core p) {
     instruction_handler_t handler = NULL;
     
     // We fetch the instruction
+	debug("fetch\n");
     result = arm_fetch(p, &instruction);
+
     if (result) {
         return result;
     }
+
     
+	debug("instruction %x\n", instruction);
     // We check the condition
     cond_field = instruction_get_condition_field(instruction);
     result = instruction_check_condition(p, cond_field);
+	debug("condition : %x, %d\n", cond_field, result);
+	
     if (result == 1) {
+		debug("condition valide\n");
         // The condition was checked, the instruction will be handled
         ins_class_field = instruction_get_handler_field(instruction);
+		debug("handler : %x\n", ins_class_field);
+
         handler = instruction_field_get_handler(ins_class_field);
     } else if (result == -1) {
         // The condition is undefined
+		debug("condition non definie\n");
         handler = arm_miscellaneous;
     }
     

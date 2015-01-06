aaf81e87fa8d2666ddc38fb9c54e212c86ff816d
juliesouchet <souchet0julie@gmail.com>
2015-01-06 15:55:11 +0100

    Data processing : parsing

--- arm_data_processing.c
+++ arm_data_processing.c
@@ -28,12 +28,83 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 #include "debug.h"
 
-// Decoding functions for different classes of instructions
+typedef enum {AND, EOR, SUB, RSB, ADD, ADC, SBC, RSC, TST, TEQ, CMP, CMN, ORR, MOV, BIC, MVN} op_code_t;
+typedef enum {LSL, LSR, ASR, ROR} shift_code_t;
+typedef int(* dp_instruction_handler_t)(arm_core, uint8_t, uint32_t, uint32_t, uint8_t);
 
+uint8_t rd, rn, rm, S, rs, shift_imm, shift_code, bit4, bit7;
+int op1, op2;
+
+// Data processing instruction parsing
+int get_op_code(uint32_t ins) {
+	return (ins >> 12) & 15;
+}
+uint8_t get_rd(uint32_t ins) {
+	return (ins >> 12) & 15;
+}
+uint8_t get_rn(uint32_t ins) {
+	return (ins >> 16) & 15;
+}
+uint8_t get_rm(uint32_t ins) {
+	return ins & 15;
+}
+uint8_t get_S(uint32_t ins) {
+	return (ins >> 20) & 1;
+}
+uint8_t get_rs(uint32_t ins) {
+	return (ins >> 8) & 15;
+}
+uint8_t get_shift_imm(uint32_t ins) {
+	return (ins >> 8) & 31;
+}
+int get_shift_code(uint32_t ins) {
+	return (ins >> 5) & 3;
+}
+uint8_t get_bit4(uint32_t ins) {
+	return (ins >> 4) & 1;
+}
+uint8_t get_bit7(uint32_t ins) {
+	return (ins >> 7) & 1;
+}
+// Immediate operand value
+int get_immediate(uint32_t ins) {
+	int imm_8 = ins & 255;
+	int rotate_imm = (ins >> 8) & 15;
+	return imm_8 >> (rotate_imm * 2) ;
+}
+// Shifted register operand value
+
+
+
+// Decoding
+dp_instruction_handler_t decode(op_code_t op_code) {
+	return and; // à compléter
+}
+
+// Decoding functions for different classes of instructions
 int arm_data_processing_shift(arm_core p, uint32_t ins) {
     debug("arm_data_processing_shift: %d\n", (int)ins);
+    
+    op_code_t op_code = get_op_code(ins);
+    rd = get_rd(ins);
+    if(op_code != MOV && op_code != MVN) {
+    	rn = get_rn(ins);
+    	op1 = arm_read_register(p, rn);
+    }
+    rm = get_rm(ins);
+    op2 = arm_read_register(p, rm);
+    if(op_code == CMP || op_code == CMN  || op_code == TST || op_code == TEQ) {
+    	S == 1;
+    }
+    else {
+    	S == get_S(ins);
+    }
+    
+    dp_instruction_handler_t handler = decode(op_code);
+    handler(p, rd, op1, op2, S);
     return UNDEFINED_INSTRUCTION;
 }
+
 
 int arm_data_processing_immediate(arm_core p, uint32_t ins) {
     debug("arm_data_processing_immediate: %d\n", (int)ins);

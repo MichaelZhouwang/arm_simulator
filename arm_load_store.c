d8b795d653dab39a65ff7d6debdc4782d7266ffe
jeremyvizzini <contact@jeremyvizzini.com>
2015-01-06 17:28:03 +0100

    implementation partielle de ldr et str

--- arm_load_store.c
+++ arm_load_store.c
@@ -27,14 +27,165 @@ Contact: Guillaume.Huard@imag.fr
 #include "util.h"
 #include "debug.h"
 
+// Get different parts of instruction
+
+static inline int arm_ls_get_p(uint32_t ins) {
+	return (int)((ins >> 24) & 1);
+}
+
+static inline int arm_ls_get_u(uint32_t ins) {
+	return (int)((ins >> 23) & 1);
+}
+
+static inline int arm_ls_get_b(uint32_t ins) {
+	return (int)((ins >> 22) & 1);
+}
+
+static inline int arm_ls_get_w(uint32_t ins) {
+	return (int)((ins >> 21) & 1);
+}
+
+static inline int arm_ls_get_l(uint32_t ins) {
+	return (int)((ins >> 20) & 1);
+}
+
+static inline uint8_t arm_ls_get_rn(uint32_t ins) {
+	return (uint8_t)((ins >> 16) & 0xF);
+}
+
+static inline uint8_t arm_ls_get_rd(uint32_t ins) {
+	return (uint8_t)((ins >> 12) & 0xF);
+}
+
+static inline uint8_t arm_ls_get_rm(uint32_t ins) {
+	return (uint8_t)(ins & 0xF);
+}
+
+// Compute offset according to manual p458
+
+// [<Rn>, #+/-<offset_12>]
+static uint32 arm_ls_compute_imm_offset(arm_core p, uint32_t ins) {	
+	uint32_t rn = arm_ls_get_rn(ins);
+	uint32_t offset = ins & 0x7FF;
+
+	if (arm_ls_get_u(ins)) { 
+		return rn + offset;
+	} else {
+	 	return rn - offset;
+	}
+}
+
+// [<Rn>, +/-<Rm>]
+// [<Rn>, +/-<Rm>, LSL #<shift_imm>]
+// [<Rn>, +/-<Rm>, LSR #<shift_imm>]
+// [<Rn>, +/-<Rm>, ASR #<shift_imm>]
+// [<Rn>, +/-<Rm>, ROR #<shift_imm>]
+// [<Rn>, +/-<Rm>, RRX]
+static uint32 arm_ls_compute_reg_offset(arm_core p, uint32_t ins) {
+	uint32_t rn = arm_ls_get_rn(ins);
+	uint32_t rm = arm_ls_get_rm(ins);
+	int shift = (ins >> 5) & 3;
+	int shift_imm = (ins >> 7) & 15;
+	int index;
+
+	// TODO
+	index = 
+
+	if (arm_ls_get_u(ins)) {
+		address = Rn + index;
+	} else {
+		address = Rn - index;
+	}
+
+// [<Rn>, #+/-<offset_12>]!
+static uint32 arm_ls_compute_imm_pre(arm_core p, uint32_t ins) {
+	uint32_t rn = arm_ls_get_rn(ins);
+	uint32_t offset = ins & 0x7FF;
+	uint32_t address;
+
+	if (arm_ls_get_u(ins)) { 
+		address = rn + offset;
+	} else {
+	 	address = rn - offset;
+	}
+	arm_write_register(p, rn, address);	
+
+	return address;
+}
+
+static uint32 arm_ls_compute_reg_pre(arm_core p, uint32_t ins) {
+
+}
+
+// [<Rn>], #+/-<offset_12>
+static uint32 arm_ls_compute_imm_post(arm_core p, uint32_t ins) {
+
+}
+
+static uint32 arm_ls_compute_reg_post(arm_core p, uint32_t ins) {
+
+}
+
+// Public
+
 int arm_load_store(arm_core p, uint32_t ins) {
     debug("arm_load_store: %d\n", (int)ins);
-    return UNDEFINED_INSTRUCTION;
+
+	int rd = arm_ls_get_rd(ins);
+	int rn = arm_ls_get_rn(ins);
+	
+	if (rn == 15) {
+		return UNDEFINED_INSTRUCTION;
+	}
+
+	if (arm_ls_get_p(ins)) {
+		address = arm_ls_compute_reg_post(p, ins);
+	} else if (arm_ls_get_w(ins)) {
+		address = arm_ls_compute_reg_pre(p, ins);
+	} else {
+		address = arm_ls_compute_reg_offset(p, ins);	
+	}
+
+	if (arm_ls_get_l(ins)) {
+		result = arm_read_word(p, address, &value);
+		if (result) arm_write_register(p, rd, value);
+	} else { 
+		value = arm_read_register(p, rd);
+		result = arm_write_word(p, address, value);
+	}
+
+    return result;
 }
 
 int arm_load_store_immediate(arm_core p, uint32_t ins) {
     debug("arm_load_store_immediate: %d\n", (int)ins);
-    return UNDEFINED_INSTRUCTION;
+	
+	uint32 address, value;
+	int result;
+	uint8_t rd = arm_ls_get_rd(ins);
+	uint8_t rn = arm_ls_get_rn(ins);
+
+	if (rn == 15) {
+		return UNDEFINED_INSTRUCTION;
+	}
+
+	if (arm_ls_get_p(ins)) {	
+		address = arm_ls_compute_imm_post(p, ins);
+	} else if (arm_ls_get_w(ins)) {
+		address = arm_ls_compute_imm_pre(p, ins);
+	} else {
+		address = arm_ls_compute_imm_offset(p, ins);
+	}
+
+	if (arm_ls_get_l(ins)) {
+		result = arm_read_word(p, address, &value);
+		if (result) arm_write_register(p, rd, value);
+	} else {
+		value = arm_read_register(p, rd);
+		result = arm_write_word(p, address, value);
+	}
+
+    return result;
 }
 
 int arm_load_store_multiple(arm_core p, uint32_t ins) {

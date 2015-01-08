89f98799cffc51d53fb29149e50095fa5a999284
jeremyvizzini <contact@jeremyvizzini.com>
2015-01-08 11:35:32 +0100

    fusion de la fonction arm_load_store et load_store_immediate

--- arm_load_store.h
+++ arm_load_store.h
@@ -28,7 +28,6 @@ Contact: Guillaume.Huard@imag.fr
 #include "arm_core.h"
 
 int arm_load_store(arm_core p, uint32_t ins);
-int arm_load_store_immediate(arm_core p, uint32_t ins);
 int arm_load_store_miscellaneous(arm_core p, uint32_t ins);
 int arm_load_store_multiple(arm_core p, uint32_t ins);
 int arm_coprocessor_load_store(arm_core p, uint32_t ins);
